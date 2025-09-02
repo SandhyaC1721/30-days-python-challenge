518699cd564c0f5c7c0142cd5f7a2b01dd63ff59
SandhyaC1721 <ssandhyac747@gmail.com>
2025-09-02 17:37:01 +0530

    Add files via upload

---
+++ day 3/fibanocci.py
@@ -0,0 +1,7 @@
+def fibonacci(n):
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+num = int(input("Enter number of terms in Fibonacci series: "))
+fibonacci(num)
