afe4dbc1346a19f28f465f95f28f99d9a146db37
SandhyaC1721 <ssandhyac747@gmail.com>
2025-09-08 17:51:40 +0530

    Add files via upload

---
+++ day 6/primecheck.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+# Taking input from user
+num = int(input("Enter a number: "))
+
+if is_prime(num):
+    print(f"{num} is a Prime number")
+else:
+    print(f"{num} is NOT a Prime number")
